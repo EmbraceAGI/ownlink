584799cc15fbfbbdd9c61a3801b14249fe2c229c
Gary_Hou <52512824+hougarry@users.noreply.github.com>
2023-09-07 22:48:08 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -37,6 +37,7 @@ export const PageHead: React.FC<
 
   return (
     <Head>
+      <GoogleAnalyticsScript />
       <meta charSet='utf-8' />
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
       <meta
